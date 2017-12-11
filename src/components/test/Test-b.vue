9a798e7d627a42dbfb782233efbf9f1df89a877c
yanchuan <yanchuan@ehousechina.com>
2017-12-11 14:44:07 +0800

    update

--- src/components/test/Test-b.vue
+++ src/components/test/Test-b.vue
@@ -3,16 +3,30 @@
 		<h1>this is b child compontent!!!</h1>
 		<h3>{{msg}}</h3>
 		<button class="btn btn-default" @click="promiseTest">promiseTest</button>
+
+		<div class="ur">
+			<ul>
+				<li v-for="item in data"><img v-bind:src="item.url" /> {{item.name}} age is {{item.age}}</li>
+			</ul>
+		</div>
 	</div>
 	
 </template>
 <script type="text/javascript">
-	
+	let Mock = require('mockjs');
 	export default {
 		data () {
 			return {
-				msg:"testb"
+				msg:"testb",
+				data:{}
 			}
+		},
+		mounted () {
+			let self = this;
+			this.axios.get('testb.json').then((res) => {
+				// console.log(res.data.data);
+				self.data = res.data.data;
+			}).catch((error) => {console.log(error);})
 		},
 		methods:{
 			promiseTest:function (){
@@ -52,11 +66,20 @@
 				  },1000);
 				});
 				Promise.all(promises).then((results) => {
-					console.log(results);
+					console.log("promiseall"+results);
 				}).catch((reason) => {
 					console.log(reason);
 				});
 			}
 		}
 	}
+	Mock.mock('testb.json',{
+		'data|1-3':[
+			{
+				"name|1-3":'yan',
+				"age|1-60":27,
+				"url":'@IMG(100x100,#50B347,#fff,YAN)'
+			}
+		]
+	});
 </script>
